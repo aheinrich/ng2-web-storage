f409774ab244e4841e3563458f245c2db72b57fb
Aaron Heinrich <nerdx7@gmail.com>
2016-05-22 23:47:05 -0700

    - adding barrel

---
+++ src/app/alerts/index.ts
@@ -0,0 +1,4 @@
+import { AlertDetailsComponent } from './alert-details.component'
+import { AlertListComponent } from './alert-list.component'
+
+export const ALERT_DIRECTIVES = [AlertDetailsComponent, AlertListComponent]
